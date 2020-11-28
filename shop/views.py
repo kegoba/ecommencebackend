f5a99c03ba438ba7cb89ecb31fa1e7bc924e0425
kegoba <egobakelvin@gmail.com>
2020-11-28 16:55:39 +0100

    update

--- shop/views.py
+++ shop/views.py
@@ -161,8 +161,7 @@ def GetWomenCategory(request):
 @api_view(["POST", "GET"])
 def Payment(request):
     if request.method == "POST":
-        print(request.data, request.data.email, request.data.id )
-        print(request.event)
+        print(request.data, request.email)
         return Response(request.data, status= status.HTTP_200_OK)
     return Response(request.errors, status= status.HTTP_400_BAD_REQUEST)
 
