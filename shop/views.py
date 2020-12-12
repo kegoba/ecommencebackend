004431944c316d553677b99c3b0fbacb8c3df24c
kegoba <egobakelvin@gmail.com>
2020-12-12 13:59:54 +0100

    paystack webhook

--- shop/views.py
+++ shop/views.py
@@ -167,7 +167,7 @@ def GetWomenCategory(request):
 def Payment(request):
     paystack_sk = "sk_fromthepaystackguys"
     customer_data = json.loads(request.body)
-    print("customer date",customer_data ,"json data ", json.loads(request.body))
+    print("customer data",customer_data,"event   event ", customer_data.event )
     computed_hmac = hmac.new(
         bytes(paystack_sk, 'utf-8'),
     str.encode(request.body.decode('utf-8')),
